4e943b2d50876f8d861dcdfded28c46060b48925
itsumura-h <dumblepy@gmail.com>
2019-10-04 13:44:31 +0900

    add frontend

--- src/project/settings.py
+++ src/project/settings.py
@@ -77,22 +77,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": 'ddd_sample_db',
-        "USER": 'root',
-        "PASSWORD": 'password',
-        "HOST": 'rdb',
-        "PORT": 5432
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": 'ddd_sample_db',
+#         "USER": 'root',
+#         "PASSWORD": 'password',
+#         "HOST": 'rdb',
+#         "PORT": 5432
+#     }
+# }
 
 
 # Password validation
